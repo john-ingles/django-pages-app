dfdf7f2a0b276a2fa57faf416f3c9841cb6bb5ac
John Ingles <john.kenneth.ingles@gmail.com>
2022-07-20 11:51:39 -0400

    Add about page template, view, and url

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,8 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = "home.html"
+    template_name: str = "home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name: str = "about.html"
